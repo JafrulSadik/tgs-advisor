8135a3804bf05300a813df5502701fbfea44a519
JafrulSadik <jafrul.sadik4@gmail.com>
2026-02-05 19:03:26 +0600

    Add changes

--- app/gallery/components/image-grid.tsx
+++ app/gallery/components/image-grid.tsx
@@ -33,74 +33,88 @@ export default function ImageGrid({ images }: { images: GalleryImageType[] }) {
                 width={400}
                 height={400}
                 className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-                src={images?.[0].image}
-                alt={images?.[0].name}
+                src={images?.[0]?.image ?? ""}
+                alt={images?.[0]?.name ?? ""}
               />
             </div>
           )}
-          <div className="col-start-1 col-end-5 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[1].image}
-              alt={images?.[1].name}
-            />
-          </div>
-          <div className="col-start-5 col-end-9 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images[2].image}
-              alt={images[2].name}
-            />
-          </div>
-          <div className="col-start-9 col-end-13 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[3]?.image}
-              alt={images?.[3]?.name}
-            />
-          </div>
-          <div className="col-start-1 col-end-5 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[4]?.image}
-              alt={images?.[4]?.name}
-            />
-          </div>
-          <div className="col-start-5 col-end-9 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[5]?.image}
-              alt={images?.[5]?.name}
-            />
-          </div>
-          <div className="col-start-9 col-end-13 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[6]?.image}
-              alt={images?.[6]?.name}
-            />
-          </div>
-          <div className="col-start-5 col-end-9 row-start-10 row-end-13 aspect-square border-4 md:border-6 lg:border-12 border-black overflow-hidden">
-            <Image
-              width={400}
-              height={400}
-              className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
-              src={images?.[7]?.image}
-              alt={images?.[7]?.name}
-            />
-          </div>
+          {images?.[1] && (
+            <div className="col-start-1 col-end-5 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[1]?.image ?? ""}
+                alt={images?.[1]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[2] && (
+            <div className="col-start-5 col-end-9 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[2]?.image ?? ""}
+                alt={images?.[2]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[3] && (
+            <div className="col-start-9 col-end-13 row-start-4 row-end-7 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[3]?.image ?? ""}
+                alt={images?.[3]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[4] && (
+            <div className="col-start-1 col-end-5 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[4]?.image ?? ""}
+                alt={images?.[4]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[5] && (
+            <div className="col-start-5 col-end-9 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-blue overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[5]?.image ?? ""}
+                alt={images?.[5]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[6] && (
+            <div className="col-start-9 col-end-13 row-start-7 row-end-10 aspect-square border-4 md:border-6 lg:border-12 border-cyan overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[6]?.image ?? ""}
+                alt={images?.[6]?.name ?? ""}
+              />
+            </div>
+          )}
+          {images?.[7] && (
+            <div className="col-start-5 col-end-9 row-start-10 row-end-13 aspect-square border-4 md:border-6 lg:border-12 border-black overflow-hidden">
+              <Image
+                width={400}
+                height={400}
+                className="object-cover w-full h-full hover:scale-110 transition-all duration-700"
+                src={images?.[7]?.image ?? ""}
+                alt={images?.[7]?.name ?? ""}
+              />
+            </div>
+          )}
         </div>
       ) : (
         <div className="text-center text-xl font-semibold text-amber-700 h-96 flex items-center justify-center">
