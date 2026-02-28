8c677415e0f15c730e2e20ed6652deeb1803d5df
JafrulSadik <jafrul.sadik4@gmail.com>
2026-02-28 16:02:11 +0600

    Add port in db config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,6 +10,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: parseInt(process.env.DATABASE_PORT || "3306"),
 });
 
 export const prisma =
